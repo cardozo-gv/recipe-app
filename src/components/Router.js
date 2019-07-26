a81d02783f72d0ae9953d92dc2c58f37c543b83f
Your Name <griseldavcardozo@gmail.com>
2019-07-26 19:59:42 -0300

    router

--- src/components/Router.js
+++ src/components/Router.js
@@ -8,7 +8,7 @@ import Recipe from './Recipe';
 const Router = () =>(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} exact/>
+      <Route path="recipe-app/" component={App} exact/>
       <Route path="/recipe/:id" component={Recipe}/>
     </Switch>
   </BrowserRouter>
