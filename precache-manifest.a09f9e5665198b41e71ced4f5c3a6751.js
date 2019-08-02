26357671b8fec3dcd642773ff24342df9065dbf5
Your Name <griseldavcardozo@gmail.com>
2019-08-02 18:04:08 -0300

    Updates

---
+++ precache-manifest.a09f9e5665198b41e71ced4f5c3a6751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a508d72d36b6f688223598ed825e5b79",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "46ab914b636b982d6e0a",
+    "url": "/recipe-app/static/css/main.d0ff66e3.chunk.css"
+  },
+  {
+    "revision": "9f37f035af8184fff8bb",
+    "url": "/recipe-app/static/js/2.3260b554.chunk.js"
+  },
+  {
+    "revision": "46ab914b636b982d6e0a",
+    "url": "/recipe-app/static/js/main.c91b7993.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
